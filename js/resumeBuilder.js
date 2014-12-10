67739ca9f48f438b23c4c6bc1f2dc9e444f76b9e
Aditya Gorti <adityagorti6@gmail.com>
2014-12-10 14:34:11 -0800

    added name and title to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Aditya Gorti");
+var formattedName = HTMLheaderName.replace("%data%", "Aditya Gorti");
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
